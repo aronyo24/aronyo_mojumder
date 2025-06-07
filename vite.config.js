6971bf42da9b4c74b22c39073d870e88287d6e90
Aronyo Mojumder <114594204+aronyo24@users.noreply.github.com>
2025-06-07 13:44:15 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/react-portfolio-template/',
+    base: '/aronyo_mojumder/',
     plugins: [react()],
     build: {
         rollupOptions: {
